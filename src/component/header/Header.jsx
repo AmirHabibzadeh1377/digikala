7971c32cd5442f6431931a622cb419b1fed2c89c
Amir Habibzadeh <amirhabibzadeh1350@gmail.com>
2023-08-14 09:41:22 +0330

    added bottom header by amirhabibzadeh in the degikala project

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./header.scss";
 import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
 const Header = () => {
   return (
     <div className='header'>
@@ -15,6 +16,7 @@ const Header = () => {
         <div className="right">
           <div>
              {<Person2OutlinedIcon className ='icon'/>}
+             {<ArrowDropDownOutlinedIcon className='arrowIcon'/>}
           </div>
           <span className='horizontal'></span>
           <div>
@@ -25,7 +27,25 @@ const Header = () => {
           </div> 
        </div>
       </div>
-      <div className="bottom"></div>
+      <div className="bottom">
+        <div className='right'>
+          <div className='navCategories'>
+            <ul className='caltegoryItems'>
+              <li className="categoryItem"> <a href="">item1</a> </li>
+              <li className="categoryItem"> <a href="">item2</a> </li>
+              <li className="categoryItem"> <a href="">item3</a> </li>
+              <li className="categoryItem"> <a href="">item4</a> </li>
+              <li className="categoryItem"> <a href="">item5</a> </li>
+              <li className="categoryItem"> <a href="">item6</a> </li>
+              <li className="categoryItem"> <a href="">item7</a> </li>
+              <li className="categoryItem"> <a href="">item8</a> </li>
+            </ul>
+          </div>
+        </div>
+        <div className="left">
+          
+        </div>
+      </div>
     </div>
   )
 }
